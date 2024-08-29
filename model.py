6268970b474af1d03c50b29dacef7689734cffcc
philiphan0109 <philiphan0109@hotmail.com>
2024-08-28 22:19:30 -0400

    push

--- model.py
+++ model.py
@@ -21,63 +21,71 @@ def init_weights(model):
 
 
 class Generator(nn.Module):
-    def __init__(self, d_input, d_features):
+    def __init__(self, d_input, d_features, img_size):
         super(Generator, self).__init__()
+        self.img_size = img_size
         self.layers = nn.Sequential(
-            # input z is shape d_input
-            nn.ConvTranspose2d(d_input, d_features*8, 4, 1, 0, bias=False),
+            # input z is shape (d_input, 1, 1)
+            nn.ConvTranspose2d(d_input, d_features*16, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(d_features*16),
+            nn.ReLU(True),
+            # State size: (d_features*16, 4, 4)
+            nn.ConvTranspose2d(d_features*16, d_features*8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(d_features*8),
             nn.ReLU(True),
-            # (d_features * 8, 4, 4)
+            # State size: (d_features*8, 8, 8)
             nn.ConvTranspose2d(d_features*8, d_features*4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(d_features*4),
             nn.ReLU(True),
-            # (d_features * 4, 8, 8)
+            # State size: (d_features*4, 16, 16)
             nn.ConvTranspose2d(d_features*4, d_features*2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(d_features*2),
             nn.ReLU(True),
-            # (d_features * 2, 16, 16)
+            # State size: (d_features*2, 32, 32)
             nn.ConvTranspose2d(d_features*2, d_features, 4, 2, 1, bias=False),
             nn.BatchNorm2d(d_features),
             nn.ReLU(True),
-            # (d_features, 32, 32)
+            # State size: (d_features, 64, 64)
             nn.ConvTranspose2d(d_features, 3, 4, 2, 1, bias=False),
             nn.Tanh()
-            # (num_channels (3), 64, 64)
+            # State size: (3, 128, 128)
         )
+        self.final_upsample = nn.Upsample(size=img_size, mode='bilinear', align_corners=False)
 
     def forward(self, x):
-        return self.layers(x)
-
+        x = self.layers(x)
+        return self.final_upsample(x)
 
 class Discriminator(nn.Module):
-    def __init__(self, num_channels, d_features):
+    def __init__(self, num_channels, d_features, img_size):
         super(Discriminator, self).__init__()
+        self.initial_downsample = nn.Upsample(size=(128, 128), mode='bilinear', align_corners=False)
         self.layers = nn.Sequential(
-            # Input is (N, num_channels, 64, 64)
+            # Input is (N, num_channels, 128, 128)
             nn.Conv2d(num_channels, d_features, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # Output is (N, d_features, 32, 32)
-            
+            # State size: (d_features, 64, 64)
             nn.Conv2d(d_features, d_features*2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(d_features*2),
             nn.LeakyReLU(0.2, inplace=True),
-            # Output is (N, d_features*2, 16, 16)
-            
+            # State size: (d_features*2, 32, 32)
             nn.Conv2d(d_features*2, d_features*4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(d_features*4),
             nn.LeakyReLU(0.2, inplace=True),
-            # Output is (N, d_features*4, 8, 8)
-            
+            # State size: (d_features*4, 16, 16)
             nn.Conv2d(d_features*4, d_features*8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(d_features*8),
             nn.LeakyReLU(0.2, inplace=True),
-            # Output is (N, d_features*8, 4, 4)
-            
-            nn.Conv2d(d_features*8, 1, 4, 1, 0, bias=False),
+            # State size: (d_features*8, 8, 8)
+            nn.Conv2d(d_features*8, d_features*16, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(d_features*16),
+            nn.LeakyReLU(0.2, inplace=True),
+            # State size: (d_features*16, 4, 4)
+            nn.Conv2d(d_features*16, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
             # Output is (N, 1, 1, 1)
         )
 
     def forward(self, input):
-        return self.layers(input)
+        x = self.initial_downsample(input)
+        return self.layers(x)
--- train.py
+++ train.py
@@ -13,133 +13,147 @@ import matplotlib.pyplot as plt
 from model import init_weights
 from model import Generator, Discriminator
 
-data = "data/dataset/"
-os.makedirs("artifacts/checkpoints", exist_ok=True)
-os.makedirs("artifacts/losses", exist_ok=True)
 
-# random seed for reproducability
-seed = 999
-random.seed(seed)
-torch.manual_seed(seed)
+def main():
+    data = "data/"
+    os.makedirs("artifacts/checkpoints", exist_ok=True)
+    os.makedirs("artifacts/losses", exist_ok=True)
 
-# hyperparameters
-batch_size = 128
-num_epochs = 40
-image_size = 64
-nc = 3 # num channels
-nz = 100 # size of latent vector
-ngf = 64 # num feature maps in generator
-ndf = 64 # num feature maps in the discriminator
-lr = 0.0001
+    # random seed for reproducability
+    seed = 999
+    random.seed(seed)
+    torch.manual_seed(seed)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # hyperparameters
+    batch_size = 128
+    num_epochs = 100
+    image_width = 184
+    image_height = 256
+    nc = 3 # num channels
+    nz = 100 # size of latent vector
+    ngf = 64 # num feature maps in generator
+    ndf = 64 # num feature maps in the discriminator
+    lr = 0.0001
 
-dataset = torchvision.datasets.ImageFolder(
-    root=data,
-    transform=transforms.Compose([
-        transforms.Resize(image_size),
-        transforms.CenterCrop(image_size),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# setup model
-generator = Generator(d_input=nz, d_features=ngf).to(device)
-discriminator = Discriminator(num_channels=nc, d_features=ndf).to(device)
-
-# load in model
-generator_checkpoint = torch.load('artifacts/checkpoints/generator_6159.pth')
-discriminator_checkpoint = torch.load('artifacts/checkpoints/discriminator_6159.pth')
-
-generator.load_state_dict(generator_checkpoint['model_state_dict'])
-discriminator.load_state_dict(discriminator_checkpoint['model_state_dict'])
-
-optimizerG = optim.Adam(generator.parameters(), lr=lr, betas=(0.5, 0.999))
-optimizerD = optim.Adam(discriminator.parameters(), lr=lr, betas=(0.5, 0.999))
-
-optimizerG.load_state_dict(generator_checkpoint['optimizer_state_dict'])
-optimizerD.load_state_dict(discriminator_checkpoint['optimizer_state_dict'])
-
-# loss function and optimizer
-criterion = nn.BCELoss()
-rand_noise = torch.randn(64, nz, 1, 1, device=device)
-
-real_label = 1.
-fake_label = 0.
-
-# training loop
-imgs_generated = []
-gen_losses = []
-disc_losses = []
-iters = 0
-
-print("Starting Training...")
-for epoch in range(num_epochs):
-    for i, data in enumerate(dataloader, 0):
-        # train the discriminator
-        discriminator.zero_grad()
-        real_imgs = data[0].to(device)
-        label = torch.full((real_imgs.size(0),), real_label, dtype=torch.float, device=device)
-        output = discriminator(real_imgs).view(-1)
-        lossd_real = criterion(output, label)
-        lossd_real.backward()
-        
-        D_x = output.mean().item()
-        
-        noise = torch.randn(real_imgs.size(0), nz, 1, 1, device=device)
-        fake_imgs = generator(noise)
-        label.fill_(fake_label)
-        output = discriminator(fake_imgs.detach()).view(-1)
-        lossd_fake = criterion(output, label)
-        lossd_fake.backward()
-        
-        D_G_z1 = output.mean().item()
-        
-        lossd = lossd_fake + lossd_real
-        optimizerD.step()
-        
-        # train the generator
-        generator.zero_grad()
-        label.fill_(real_label)
-        output = discriminator(fake_imgs).view(-1)
-        lossg = criterion(output, label)
-        lossg.backward()
-        D_G_z2 = lossg.mean().item()
-        
-        optimizerG.step()
-        
-        gen_losses.append(lossg.item())
-        disc_losses.append(lossd.item())
-        
-        print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f' % (epoch, num_epochs, i, len(dataloader), lossd.item(), lossg.item(), D_x, D_G_z1, D_G_z2))
+    dataset = torchvision.datasets.ImageFolder(
+        root=data,
+        transform=transforms.Compose([
+            transforms.Resize((image_height, image_width)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    )
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
     
-        if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
-            torch.save({
-                'epoch': epoch,
-                'model_state_dict': generator.state_dict(),
-                'optimizer_state_dict': optimizerG.state_dict(),
-            }, f'artifacts/checkpoints/generator_continued_{iters}.pth')
+    # setup model
+    generator = Generator(d_input=nz, d_features=ngf, img_size=(image_height, image_width)).to(device)
+    discriminator = Discriminator(num_channels=nc, d_features=ndf, img_size=(image_height, image_width)).to(device)
 
-            torch.save({
-                'epoch': epoch,
-                'model_state_dict': discriminator.state_dict(),
-                'optimizer_state_dict': optimizerD.state_dict(),
-            }, f'artifacts/checkpoints/discriminator_continued_{iters}.pth')
+    # load in model
+    # generator_checkpoint = torch.load('artifacts/checkpoints/generator_6159.pth')
+    # discriminator_checkpoint = torch.load('artifacts/checkpoints/discriminator_6159.pth')
+
+    # generator.load_state_dict(generator_checkpoint['model_state_dict'])
+    # discriminator.load_state_dict(discriminator_checkpoint['model_state_dict'])
+
+
+    # loss function and optimizer
+    optimizerG = optim.Adam(generator.parameters(), lr=lr, betas=(0.5, 0.999))
+    optimizerD = optim.Adam(discriminator.parameters(), lr=lr, betas=(0.5, 0.999))
+
+    # optimizerG.load_state_dict(generator_checkpoint['optimizer_state_dict'])
+    # optimizerD.load_state_dict(discriminator_checkpoint['optimizer_state_dict'])
+
+    criterion = nn.BCELoss()
+
+    real_label = 1.
+    fake_label = 0.
+
+    # training loop
+    imgs_generated = []
+    gen_losses = []
+    disc_losses = []
+    iters = 0
+
+    print("Starting Training...")
+    for epoch in range(num_epochs):
+        for i, data in enumerate(dataloader, 0):
+            # train the discriminator
+            discriminator.zero_grad()
+            real_imgs = data[0].to(device)
+            label = torch.full((real_imgs.size(0),), real_label, dtype=torch.float, device=device)
+            output = discriminator(real_imgs).view(-1)
+            lossd_real = criterion(output, label)
+            lossd_real.backward()
             
-            with torch.no_grad():
-                fake = generator(torch.randn(64, nz, 1, 1, device=device)).detach().cpu()
-            imgs_generated.append(torchvision.utils.make_grid(fake, padding=2, normalize=True))
-
-        if iters % 1000 == 0:
-            plt.figure(figsize=(10, 5))
-            plt.plot(gen_losses, label="generator losses")
-            plt.plot(disc_losses, label="discriminator losses")
-            plt.xlabel("Iterations")
-            plt.ylabel("Loss")
-            plt.legend()
-            plt.savefig(f'artifacts/losses/loss_plot_continued_{iters}.png')
-            plt.close()
+            D_x = output.mean().item()
+            
+            noise = torch.randn(real_imgs.size(0), nz, 1, 1, device=device)
+            fake_imgs = generator(noise)
+            label.fill_(fake_label)
+            output = discriminator(fake_imgs.detach()).view(-1)
+            lossd_fake = criterion(output, label)
+            lossd_fake.backward()
+            
+            D_G_z1 = output.mean().item()
+            
+            lossd = lossd_fake + lossd_real
+            optimizerD.step()
+            
+            # train the generator
+            generator.zero_grad()
+            label.fill_(real_label)
+            output = discriminator(fake_imgs).view(-1)
+            lossg = criterion(output, label)
+            lossg.backward()
+            D_G_z2 = lossg.mean().item()
+            
+            optimizerG.step()
+            
+            gen_losses.append(lossg.item())
+            disc_losses.append(lossd.item())
+            
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f' % (epoch, num_epochs, i, len(dataloader), lossd.item(), lossg.item(), D_x, D_G_z1, D_G_z2))
+            
+            iters += 1
         
-        iters += 1
+        # Save checkpoints at the end of each epoch
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': generator.state_dict(),
+            'optimizer_state_dict': optimizerG.state_dict(),
+        }, f'artifacts/checkpoints/generator_epoch_{epoch}.pth')
+
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': discriminator.state_dict(),
+            'optimizer_state_dict': optimizerD.state_dict(),
+        }, f'artifacts/checkpoints/discriminator_epoch_{epoch}.pth')
+        
+        # Generate and save sample images at the end of each epoch
+        with torch.no_grad():
+            fake = generator(torch.randn(64, nz, 1, 1, device=device)).detach().cpu()
+        img_grid = torchvision.utils.make_grid(fake, padding=2, normalize=True)
+        imgs_generated.append(img_grid)
+        
+        plt.figure(figsize=(15, 15))
+        plt.imshow(np.transpose(img_grid, (1, 2, 0)))
+        plt.axis('off')
+        plt.title(f"Generated Images - Epoch {epoch}")
+        plt.savefig(f'artifacts/generated_images/generated_image_epoch_{epoch}.png')
+        plt.close()
+
+        # Save loss plot at the end of each epoch
+        plt.figure(figsize=(10, 5))
+        plt.plot(gen_losses, label="generator losses")
+        plt.plot(disc_losses, label="discriminator losses")
+        plt.xlabel("Iterations")
+        plt.ylabel("Loss")
+        plt.legend()
+        plt.savefig(f'artifacts/losses/loss_plot_epoch_{epoch}.png')
+        plt.close()
+        
+if __name__ == "__main__":
+    main()
